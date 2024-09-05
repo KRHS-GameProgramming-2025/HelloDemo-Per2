27b418e242cc30ddcac0010d19fb5d91752fc054
Chris Spooner <cspooner@kearsarge.org>
2024-09-05 08:54:40 -0400

    setup

---
+++ hello.py
@@ -0,0 +1,14 @@
+name = input("What is your name? " )
+greeting = "Hey"
+print(greeting + " " + name)
+color = input("What is your favorite color? ")
+if color == "green":
+    print("Green is not a creative color!")
+elif color == "Green":
+    print("Green is not a creative color!")
+elif color == "blue":
+    print("OMG! "+ name + ", blue is my favorite color!")
+elif color == "Blue":
+    print("OMG! "+ name + ", blue is my favorite color!")
+else:
+    print("I really like " + color + " too!")
